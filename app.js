ac16fe8fd00f5c27ce130d05cd8aa65021381dca
muneebx786 <muneeb1feb@gmail.com>
2023-04-02 14:11:50 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,61 @@
+alert("26 roze reh gaye");
+
+// console.log("Ramzan or javascript");
+
+
+// var title ="Ramzan or javascript";
+// console.log( title);
+
+
+// var a = 10;
+// var b = 20; 
+// var c = 50;
+// var x =(a + b) - c
+// console.log(x);
+
+
+// var x ="MUHAMMMAD";
+// var y ="SHAHZAIB";
+
+
+// var z = x + y;
+// console.log(z)
+
+// **************poss incriment*****************
+// var a =20;
+// a++;
+// console.log(a)
+
+
+// var b =20;
+// b++;
+// console.log(b)
+
+// **************poss dicriment*****************
+
+// var a =20;
+// a--;
+// console.log(a)
+
+
+// var b =20;
+// b--;
+// console.log(b)
+
+
+// var firstname = prompt("enter your name");
+// var lastname = prompt("enter your lastname");
+// var city = prompt("enter city", "Karachi");
+// console.log(firstname + " " + lastname + " From " + city);
+
+
+var maiaarahaho = true;
+
+if (maiaarahaho == true) {
+    console.log("Ajao");
+} else{
+    console.log("Nhii ao");
+}
+
+
+
